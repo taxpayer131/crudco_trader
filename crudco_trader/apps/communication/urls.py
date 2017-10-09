94c336c69c772c842807d9c812c9d7af5ffc3573
MarMegh <martinmm@outlook.com>
2017-10-09 13:56:00 -0700

    sketched out views and url patterns

--- crudco_trader/apps/communication/urls.py
+++ crudco_trader/apps/communication/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^add', views.add, name = 'add'),
+    url(r'^create', views.create, name = 'create'),
+    url(r'^read', views.read, name = 'read'),
+    url(r'^update', views.update, name = 'update'),
+    url(r'^delete', views.delete, name = 'delete'),
     ]
